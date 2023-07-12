d7e4558ac27bf531629032370ba9c44f8c9965ff
cenkgurbet <cenk.gurbet@deepnetwork.com>
2023-07-12 20:58:03 +0200

    CG - Add Getter and Setter annotations to Data in Server.

--- SERVER/src/main/java/server/Data.java
+++ SERVER/src/main/java/server/Data.java
@@ -1,95 +1,13 @@
 package server;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class Data {
     String key;
     String value;
     String timestamp;
     int frequency;
-
-    /**
-     * Get key of data
-     *
-     * @param
-     * @return key
-     */
-    public String getKey() {
-        return key;
-    }
-
-
-    /**
-     * Get value of data
-     *
-     * @param
-     * @return value
-     */
-    public String getValue() {
-        return value;
-    }
-
-
-    /**
-     * Get timestamp of data
-     *
-     * @param
-     * @return timestamp
-     */
-    public String getTimestamp() {
-        return timestamp;
-    }
-
-
-    /**
-     * Get frequency of data
-     *
-     * @param
-     * @return frequency
-     */
-    public int getFrequency() {
-        return frequency;
-    }
-
-
-    /**
-     * Set key of data
-     *
-     * @param key
-     * @return
-     */
-    public void setKey(String key) {
-        this.key = key;
-    }
-
-
-    /**
-     * Set value of data
-     *
-     * @param value
-     * @return
-     */
-    public void setValue(String value) {
-        this.value = value;
-    }
-
-
-    /**
-     * Set timestamp of data
-     *
-     * @param timestamp
-     * @return
-     */
-    public void setTimestamp(String timestamp) {
-        this.timestamp = timestamp;
-    }
-
-
-    /**
-     * Set frequency of data
-     *
-     * @param frequency
-     * @return
-     */
-    public void setFrequency(int frequency) {
-        this.frequency = frequency;
-    }
 }
